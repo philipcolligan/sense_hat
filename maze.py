430a5d6b0c751be3a6a339692bec6b09ee0c434b
Philip Colligan <philipcolligan@users.noreply.github.com>
2016-02-08 15:45:38 +0000

    fix bug

--- maze.py
+++ maze.py
@@ -2,4 +2,4 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
-sense.show_message("Hello Ben!", scroll_speed=0.5,colour=(255,0,255))
+sense.show_message("Hello Ben!", scroll_speed=0.5,text_colour=(255,0,255))
